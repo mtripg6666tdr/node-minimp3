2a0f1ef3b6944d617f23b410ef280cc3c1722d2a
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-03-08 18:51:02 +0900

    Change: remove debugger

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
+// @ts-check
 const minimp3 = require('bindings')('minimp3')
 const { Transform } = require('stream')
-const debug = require('debug')('minimp3')
 
 class Decoder extends Transform {
-  constructor (opts) {
-    debug('Constructor')
-    super()
-  }
-
-  _transform (chunk, encoding, processed) {
-    debug(`_transform(): (${chunk.length} bytes)`)
-
+  /**
+   * @param {Buffer} chunk
+   * @param {string|undefined} _
+   * @param {() => void} processed
+   */
+  _transform (chunk, _, processed) {
     const decodedBuffer = minimp3.decodeBuffer(chunk, chunk.length)
-    debug(`decodedBuffer.length: ${decodedBuffer.length}`)
 
     if (decodedBuffer.length > 0) this.push(decodedBuffer)
 
