72c1345ef297e626d7ef54240df15b5dd3b0453c
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-03-09 00:16:19 +0900

    Feat: get audio meta data

--- index.js
+++ index.js
@@ -3,17 +3,59 @@ const minimp3 = require('bindings')('minimp3')
 const { Transform } = require('stream')
 
 class Decoder extends Transform {
+  /** @type {import('stream').TransformOptions & {maximumChunkLength: number}} */
+  constructor(options){
+    super(options);
+    this.maximumChunkLength = options.maximumChunkLength || 100000000;
+    /** @type {number} */
+    this.bitrate = 0;
+    /** @type {number} */
+    this.channels = -1;
+    /** @type {number} */
+    this.samplingRate = -1;
+  }
+
   /**
    * @param {Buffer} chunk
    * @param {string|undefined} _
-   * @param {() => void} processed
+   * @param {() => void} callback
    */
-  _transform (chunk, _, processed) {
-    const decodedBuffer = minimp3.decodeBuffer(chunk, chunk.length)
+  _transform (chunk, _, callback) {
+    /** @type {Buffer[]} */
+    const buffers = [];
 
-    if (decodedBuffer.length > 0) this.push(decodedBuffer)
+    if(chunk.length < this.maximumChunkLength){
+      buffers.push(chunk);
+    }else{
+      const chunkLength = Math.ceil(chunk.length / this.maximumChunkLength);
+      for(let i = 0; i < chunkLength; i++){
+        buffers.push(chunk.subarray(i * this.maximumChunkLength, (i + 1) * this.maximumChunkLength - 1));
+      }
+    }
 
-    processed()
+    /** @type {{buffer: Buffer, bitrate: number, channels: number, samplingRate: number}} */
+    const {
+      buffer: decodedBuffer,
+      bitrate,
+      channels,
+      samplingRate,
+    } = minimp3.decodeBuffer(chunk, chunk.length);
+
+    if(this.bitrate !== bitrate){
+      this.emit("bitrateChanged", this.bitrate = bitrate);
+    }
+    if(this.channels < 0){
+      this.emit("channelData", this.channels = channels);
+    }
+    if(this.samplingRate < 0){
+      this.emit("samplingRateData", this.samplingRate = samplingRate);
+    }
+
+    if (decodedBuffer.length > 0){
+      this.push(decodedBuffer);
+    }
+
+    callback();
   }
 }
 
--- lib/minimp3.cc
+++ lib/minimp3.cc
@@ -10,11 +10,30 @@ Napi::Value DecodeBuffer(const Napi::CallbackInfo &info) {
   mp3dec_file_info_t f_info;
 
   mp3dec_load_buf(&mp3d, buf, buf_size, &f_info, 0, 0);
-  return Napi::Buffer<mp3d_sample_t>::New(
-    info.Env(),
-    f_info.buffer,
-    f_info.samples - sizeof(mp3d_sample_t)
+
+  Napi::Env env = info.Env();
+  Napi::Object result = Napi::Object::New(info.Env());
+  result.Set(
+    Napi::String::New(env, "bitrate"),
+    f_info.avg_bitrate_kbps
   );
+  result.Set(
+    Napi::String::New(env, "channels"),
+    f_info.channels
+  );
+  result.Set(
+    Napi::String::New(env, "samplingRate"),
+    f_info.samples
+  );
+  result.Set(
+    Napi::String::New(env, "buffer"),
+    Napi::Buffer<mp3d_sample_t>::New(
+      info.Env(),
+      f_info.buffer,
+      f_info.samples - sizeof(mp3d_sample_t)
+    )
+  );
+  return result;
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
